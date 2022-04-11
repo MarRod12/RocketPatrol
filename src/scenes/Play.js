b02ab6ba876f1b43594359df89c8dec5a764d1f8
Margarita Rodriguez <mrodr157@ucsc.edu>
2022-04-10 18:29:32 -0700

    Added flying rocket to screen

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,17 +4,28 @@ class Play extends Phaser.Scene {
     }
     preload() {
         this.load.image('starfield', 'assets/starfield.png');
+        this.load.image('rocket', 'assets/rocket.png');
     }
 
-    create () {
-        let playText = this.add.text(20, 20, "This is the play scene");
+    create() {
+        
+        this.starfield = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'starfield').setOrigin(0,0);
 
+        //green bar across top of screen
         this.add.rectangle(0,borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0,0);
 
-
+        //border
         this.add.rectangle(0,0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(0,0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);
+
+
+        this.p1Rocket = new Rocket(this, game.config.width/2, 431, 'rocket').setOrigin(0.5, 0);
+    }
+
+    update() {
+        this.starfield.tilePositionX -=4;
+        this.p1Rocket.y -= 1;
     }
 }
