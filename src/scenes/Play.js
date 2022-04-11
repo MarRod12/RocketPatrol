cb41950d6e243e5c6c002f9f85fffb21ace610a1
Margarita Rodriguez <mrodr157@ucsc.edu>
2022-04-11 00:29:39 -0700

    fixed player movement and added moving spaceships

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,6 +5,7 @@ class Play extends Phaser.Scene {
     preload() {
         this.load.image('starfield', 'assets/starfield.png');
         this.load.image('rocket', 'assets/rocket.png');
+        this.load.image('spaceship', 'assets/spaceship.png');
     }
 
     create() {
@@ -29,17 +30,26 @@ class Play extends Phaser.Scene {
 
         //give our player a starting point
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
-        
+
         //define keys
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
 
-        this.p1Rocket.update();
+        //add three spaceships
+        this.ship1 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
+        this.ship2 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0, 0);
+        this.ship3 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0, 0);
     }
 
     update() {
         this.starfield.tilePositionX -=4;
+
+        this.p1Rocket.update();
+
+        this.ship1.update();
+        this.ship2.update();
+        this.ship3.update();
     }
 }
